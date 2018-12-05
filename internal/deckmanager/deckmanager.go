7b3a1bee5695db16ee9575e93f8dda445d222290
Heindrich Paul <heindrich.paul@gmail.com>
2018-12-05 10:32:03 +0100

    fixed dependencies

--- internal/deckmanager/deckmanager.go
+++ internal/deckmanager/deckmanager.go
@@ -1,46 +1,46 @@
 package deckmanager
 
-import "github.com/heindrichpaul/deckOfCards"
+import "github.com/heindrichpaul/deckofcards"
 
-var decks map[string]*deckOfCards.Deck
-var piles map[string]*deckOfCards.Pile
+var decks map[string]*deckofcards.Deck
+var piles map[string]*deckofcards.Pile
 
-func RequestNumberOfDecks(number int) *deckOfCards.Deck {
-	deck := deckOfCards.NewDeck(number)
+func RequestNumberOfDecks(number int) *deckofcards.Deck {
+	deck := deckofcards.NewDeck(number)
 	if deck.Success {
 		decks[deck.DeckID] = deck
 	}
 	return deck
 }
 
-func RequestNumberOfShuffledDecks(number int) *deckOfCards.Deck {
+func RequestNumberOfShuffledDecks(number int) *deckofcards.Deck {
 	deck := RequestNumberOfDecks(number)
 	if deck.Success {
-		deck = deckOfCards.ShuffleDeck(deck)
+		deck = deckofcards.ShuffleDeck(deck)
 		decks[deck.DeckID] = deck
 	}
 	return deck
 }
 
-func RequestNumberOfDecksWithJockers(number int) *deckOfCards.Deck {
-	deck := deckOfCards.NewDeckWithJockers(number)
+func RequestNumberOfDecksWithJockers(number int) *deckofcards.Deck {
+	deck := deckofcards.NewDeckWithJockers(number)
 	if deck.Success {
 		decks[deck.DeckID] = deck
 	}
 	return deck
 }
 
-func RequestNumberOfShuffledDecksWithJockers(number int) *deckOfCards.Deck {
+func RequestNumberOfShuffledDecksWithJockers(number int) *deckofcards.Deck {
 	deck := RequestNumberOfDecksWithJockers(number)
 	if deck.Success {
-		deck = deckOfCards.ShuffleDeck(deck)
+		deck = deckofcards.ShuffleDeck(deck)
 		decks[deck.DeckID] = deck
 	}
 	return deck
 }
 
-func ReshuffleDeck(deck *deckOfCards.Deck) *deckOfCards.Deck {
-	deck = deckOfCards.ShuffleDeck(deck)
+func ReshuffleDeck(deck *deckofcards.Deck) *deckofcards.Deck {
+	deck = deckofcards.ShuffleDeck(deck)
 	if deck.Success {
 		decks[deck.DeckID] = deck
 	}
@@ -48,15 +48,15 @@ func ReshuffleDeck(deck *deckOfCards.Deck) *deckOfCards.Deck {
 
 }
 
-func RequestSingleUnshuffledDeck() *deckOfCards.Deck {
-	deck := deckOfCards.NewDeck(1)
+func RequestSingleUnshuffledDeck() *deckofcards.Deck {
+	deck := deckofcards.NewDeck(1)
 	if deck.Success {
 		decks[deck.DeckID] = deck
 	}
 	return deck
 }
 
-func FindDeckById(Id string) *deckOfCards.Deck {
+func FindDeckById(Id string) *deckofcards.Deck {
 	deck, ok := decks[Id]
 	if !ok {
 		return nil
