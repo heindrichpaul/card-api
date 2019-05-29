e7c2e5ff773f8acdb0f5c0a26e5e4fe57accc338
Heindrich Paul <heindrichpaul@Heindrichs-MacBook-Pro.local>
2019-05-29 20:47:27 +0200

    Added better error handling and deck endpoints

--- cmd.go
+++ cmd.go
@@ -3,19 +3,98 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
+	"github.com/heindrichpaul/card-api/deckmanager"
+	"github.com/heindrichpaul/card-api/errormanager"
+	"github.com/heindrichpaul/deckofcards"
 	"github.com/rs/cors"
 )
 
-func main() {
+var (
+	deckManager *deckmanager.DeckManager = deckmanager.NewDeckManager()
+	logErrors   bool                     = false
+)
 
+func main() {
 	mux := mux.NewRouter()
-	mux.HandleFunc("/hello", HelloWorldHandler)
+	deckAPI := mux.PathPrefix("/deck").Subrouter()
+	deckAPI.Path("/new").Methods("GET").HandlerFunc(newDeckHandler)
+	deckAPI.Path("/new").Methods("GET").HandlerFunc(newDeckHandler).Queries("amount", "{amount}")
+	deckAPI.Path("/new").Methods("GET").HandlerFunc(newDeckHandler).Queries("jokers", "{jokers}")
+	deckAPI.Path("/new").Methods("GET").HandlerFunc(newDeckHandler).Queries("amount", "{amount}", "jokers", "{jokers},")
+	deckAPI.Path("/{id}").Methods("GET").HandlerFunc(retrieveDeckHandler)
 	handler := cors.Default().Handler(mux)
 	http.ListenAndServe(":8080", handler)
 }
 
-func HelloWorldHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World")
+func newDeckHandler(w http.ResponseWriter, r *http.Request) {
+	v := r.URL.Query()
+	amountOfDecks, err := strconv.Atoi(v.Get("amount"))
+	if err != nil {
+		amountOfDecks = 1
+	}
+
+	jockers, err := strconv.ParseBool(v.Get("jokers"))
+	if err != nil {
+		jockers = false
+	}
+
+	var deck *deckofcards.Deck
+	if jockers {
+		deck = deckManager.RequestNumberOfDecksWithJokers(amountOfDecks)
+	} else {
+		deck = deckManager.RequestNumberOfDecks(amountOfDecks)
+	}
+
+	deckJSON, err := deck.Marshal()
+	if err != nil {
+		handleError(w, r, "Could not marshal deck", "1", logErrors)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(deckJSON))
+}
+
+func retrieveDeckHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	deck := deckManager.FindDeckById(id)
+	if deck == nil {
+		handleError(w, r, fmt.Sprintf("Could not find deck with id: %s", id), "1", logErrors)
+		return
+	}
+
+	deckJSON, err := deck.Marshal()
+	if err != nil {
+		handleError(w, r, "Could not marshal deck", "1", logErrors)
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprintf(w, string(deckJSON))
+
+}
+
+func handleError(w http.ResponseWriter, r *http.Request, message, code string, log bool) {
+	w.Header().Set("Content-Type", "application/json")
+	if log {
+		w.WriteHeader(http.StatusInternalServerError)
+		e := errormanager.NewAPIErrorAndLog(message, code, r)
+		eJSON, err := e.Marshal()
+		if err == nil {
+			fmt.Fprintf(w, string(eJSON))
+			return
+		}
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+		e := errormanager.NewAPIError(message, code)
+		eJSON, err := e.Marshal()
+		if err == nil {
+			fmt.Fprintf(w, string(eJSON))
+			return
+		}
+	}
+
 }
