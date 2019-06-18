a75531cd0ae834e8d1762cf8ad92ea399c6dbb28
Heindrich Paul <heindrich.paul@gmail.com>
2019-06-18 13:46:38 +0000

    made the map persistence thread safe

--- manager/persistance/mapmanager.go
+++ manager/persistance/mapmanager.go
@@ -1,11 +1,17 @@
 package persistence
 
-import "github.com/heindrichpaul/deckofcards"
+import (
+	"sync"
+
+	"github.com/heindrichpaul/deckofcards"
+)
 
 //MapManager is a struct that implements the interfaces.PersistenceManager interface with a internal map as a way of persistence.
 type MapManager struct {
-	piles map[string]*deckofcards.Pile
-	decks map[string]*deckofcards.Deck
+	piles   map[string]*deckofcards.Pile
+	decks   map[string]*deckofcards.Deck
+	pileMux sync.RWMutex
+	deckMux sync.RWMutex
 }
 
 //NewMapPersistenceManager returns a pointer to a newly created MapManager.
@@ -19,24 +25,32 @@ func NewMapPersistenceManager() *MapManager {
 
 //PersistDeck persists the deck to the map.
 func (z *MapManager) PersistDeck(deck *deckofcards.Deck) bool {
+	z.deckMux.Lock()
+	defer z.deckMux.Unlock()
 	z.decks[deck.DeckID] = deck
 	return true
 }
 
 //RetrieveDeck retrieves the deck with the given ID from the map.
 func (z *MapManager) RetrieveDeck(ID string) (*deckofcards.Deck, bool) {
+	z.deckMux.RLock()
+	defer z.deckMux.RUnlock()
 	deck, ok := z.decks[ID]
 	return deck, ok
 }
 
 //PersistPile persists the pile to the map.
 func (z *MapManager) PersistPile(pile *deckofcards.Pile) bool {
+	z.pileMux.Lock()
+	defer z.pileMux.Unlock()
 	z.piles[pile.PileID] = pile
 	return true
 }
 
 //RetrievePile retrieves the pile with the given ID from the map.
 func (z *MapManager) RetrievePile(ID string) (*deckofcards.Pile, bool) {
+	z.pileMux.RLock()
+	defer z.pileMux.RUnlock()
 	pile, ok := z.piles[ID]
 	return pile, ok
 }
